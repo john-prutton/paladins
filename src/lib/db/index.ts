bd2e8d6b7d8e24da7364307c211b62d6daa50a4c
John Prutton (PRTJOH016) <johnscon9@gmail.com>
2023-10-10 19:46:58 +0200

    feat: add schema to database client

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,6 +1,14 @@
-import { drizzle, BetterSQLite3Database } from "drizzle-orm/better-sqlite3"
+import { drizzle } from "drizzle-orm/better-sqlite3"
 import Database from "better-sqlite3"
 import { env } from "../env.mjs"
 
+import * as AuthSchema from "./schema/auth"
+import * as ComputerSchema from "./schema/computers"
+
 export const sqlite = new Database(env.DATABASE_URL)
-export const db: BetterSQLite3Database = drizzle(sqlite)
+export const db = drizzle(sqlite, {
+	schema: {
+		...AuthSchema,
+		...ComputerSchema,
+	},
+})
