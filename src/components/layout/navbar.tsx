2d23d0d162aa5b1660caefaf6e9b4a6e10b9fc25
John Prutton (PRTJOH016) <johnscon9@gmail.com>
2023-10-12 15:04:46 +0200

    style(navbar)

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -3,7 +3,7 @@ import { Logo } from "@/components/layout/logo"
 
 export function Navbar() {
 	return (
-		<nav className="flex flex-row items-center justify-between py-2 shadow">
+		<nav className="mb-8 flex flex-row items-center justify-between rounded-b-lg bg-slate-100/5 py-2 shadow-xl shadow-slate-900">
 			<Logo />
 
 			<ProfileButton />
